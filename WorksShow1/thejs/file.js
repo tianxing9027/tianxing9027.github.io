76a5702d6454eeaa49c6d0687ba4c50fe962d8a2
tianxing9027 <1164617962@qq.com>
2018-08-21 16:12:39 +0800

    WorkShow1

---
+++ WorksShow1/thejs/file.js
@@ -0,0 +1,10 @@
+function over(len){           
+              for(var j = 3; j < len-8; j++) {
+              span[j].index=j;
+              span[j].className = "oneA";                 
+}      }  
+function out(len){
+      for(var j = 3; j < len-8; j++) {
+              span[j].index=j;
+              span[j].className = "one";
+}}
